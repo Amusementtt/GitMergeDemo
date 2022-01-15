e22e139cd236f22e2fc8515ac27e59454f52e6cd
王钰 <wangyu9@bonc.com.cn>
2022-01-15 22:53:15 +0800

    master分支提交

---
+++ src/main/java/com/example/gitmergedemo/GitMergeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmergedemo/GitMergeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
